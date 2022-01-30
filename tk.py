1757e183c8d26814cb2fb75ac2e46bb3c804ca99
rhemney <bostoneochieng69@gmail.com>
2022-01-30 04:24:30 -0800

    tkinter GUI

---
+++ tk.py
@@ -0,0 +1,8 @@
+import tkinter
+
+from tkinter import * #this means from tkinter you import everything
+
+win = Tk() # this is for creating a variable win for the window
+win.geometry("400x400") #this is for controlling the height and width of the application
+
+win.mainloop()
